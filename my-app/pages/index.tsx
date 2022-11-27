b6b676ec3269923d56a66900e915190dbf569446
eesh <eesh1996@gmail.com>
2022-11-27 20:01:28 +0530

    Eslint error fixed

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -238,7 +238,7 @@ export default function Home() {
     if (!presaleStarted) {
       return (
         <div>
-          <div className={styles.description}>Presale hasn't started yet!</div>
+          <div className={styles.description}>Presale hasnt started yet!</div>
         </div>
       );
     }
